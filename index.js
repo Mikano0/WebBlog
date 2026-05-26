0d56c219d48840eb991f86e267280f8d75d1f260
Wouter Hendriks <wouter.hendriks@ziggo.nl>
2026-05-26 14:09:12 +0200

    Add individual post viewing

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let posts = [{title: "First Post", content: "This is the content of the first post.",},
-            {title: "Second Post", content: "This is the content of the second post.",},
-            {title: "Third Post", content: "This is the content of the third post.",},
-            {title: "Fourth Post", content: "This is the content of the fourth post.",},
-            {title: "Fifth Post", content: "This is the content of the fifth post.",},]
+let posts = [{title: "First Post", content: "This is the content of the first post." , slug: "first-post"},
+            {title: "Second Post", content: "This is the content of the second post.", slug: "second-post"},
+            {title: "Third Post", content: "This is the content of the third post.", slug: "third-post"},
+            {title: "Fourth Post", content: "This is the content of the fourth post.", slug: "fourth-post"},
+            {title: "Fifth Post", content: "This is the content of the fifth post.", slug: "fifth-post"},]
+
+function createSlug(title){
+    return title.toLowerCase().split(" ").join("-");
+}
 
 app.get("/", (req, res) => {
     res.render("index", {posts: posts});
@@ -26,9 +30,19 @@ app.post("/", (req, res) => {
     //Handle form submission and save the post, Add database later
     const title = req.body.title;
     const content = req.body.content;
-    posts.push({title,content});
+    posts.push({title,content, slug: createSlug(title)});
     res.redirect("/");
 });
+
+app.get("/post/:slug", (req, res) => {
+    const slug = req.params.slug;
+    const post = posts.find(p => createSlug(p.title) === slug);
+    if (post){
+        res.render("post", {post: post});
+    } else {
+        res.render("post", {post: null});
+    }
+})
 app.listen(port, () => {
     console.log("Server is running on port " + port);
 });
